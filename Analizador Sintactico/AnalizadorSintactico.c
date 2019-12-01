d5de44dbde66aac9e8a9a20d41742a309a5bc507
ermeza <edwin.escalante@yahoo.com>
2019-11-30 19:00:08 -0600

    AnalizadorSintactico completo

--- Analizador Sintactico/AnalizadorSintactico.c
+++ Analizador Sintactico/AnalizadorSintactico.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include <string.h>
-#include<stderr>
 #define MAX 100
 
 //---------------------------------------------------------------------------
@@ -10,124 +9,157 @@
 struct token
 {
   char tokens[MAX];
-};
+}token1;
 struct error
 {
   char errors[MAX];
-};
+}error1;
+
 //---------------------------------------------------------------------------
 
-token token1;
-error error1;
-int int main() {
+
+int main() {
   FILE *pf,*Tokens;
   FILE *Errores;
+  int i,j;
   char c;
   char Estado=0;
-  char pal_res[]={"define", "imprimir", "leer", "Si", "SiNo", "Hacer", \
+  char pal_res[16][25]={"define", "imprimir", "leer", "Si", "SiNo", "Hacer", \
                         "FinSi", "Repetir", "Mientras", "Hasta", "entero", "flotante", "caracter"};
-  pf=fopen("ArchivoAnalisis.txt","r");
-  Tokens=fopen("Tokens.txt", "w+");
-  Errores=fopen("Errores.txt", "w+");
-  if (pf=NULL){
-    fpats("Error en el ArchivoAnalisis",stderr);
+  pf=fopen("ArchivoAnalisis.Txt","r");
+  Tokens=fopen("Tokens.Txt", "w+");
+  Errores=fopen("Errores.Txt", "w+");
+  if (pf==NULL){
+    strcpy(error1.errors, "Error en el archivo");
+    fwrite (&error1, sizeof(error1), 1, Errores);
     exit(1);
   }else{
-    while ((c=fgetc(pf))!EOF) {
-      switch (c) {
-        case ( c>=65 && c<=90 )://Letras Mayusculas
-          if (Estado==0||Estado==1||Estado==2||Estado==9) {
-            strcpy(token1.tokens, "Tkn_LetraMa");
-            fwrite (&token1, sizeof(token1), 1, Tokens);
-            Estado==1;
-          }
-          break;
-        case (c>=96 && c<=122)://Letras Minusculas
-            if (Estado==1||Estado==2) {
-              strcpy(token1.tokens, "Tkn_LetraMi");
-              fwrite (&token1, sizeof(token1), 1, Tokens);
-              Estado==1;
-            }
-            break;
-        case (c<=57&&c>=48)://Numeros
-          if (Estado==1||Estado==2) {
-            strcpy(token1.tokens, "Tkn_numero");
-            fwrite (&token1, sizeof(token1), 1, Tokens);
-            Estado==2
-          } else if (Estado==2||Estado==9) {
-            strcpy(token1.tokens, "Tkn_numero");
-            fwrite (&token1, sizeof(token1), 1, Tokens);
-            Estado==8;
-          } else if (Estado==9) {
-            strcpy(token1.tokens, "Tkn_numero");
-            fwrite (&token1, sizeof(token1), 1, Tokens);
-            Estado==5;
-          } else if (Estado==3||Estado==5) {
-            strcpy(token1.tokens, "Tkn_numero");
-            fwrite (&token1, sizeof(token1), 1, Tokens);
-            Estado==3;
-          } else if (Estado==4) {
-            strcpy(token1.tokens, "Tkn_numero");
-            fwrite (&token1, sizeof(token1), 1, Tokens);
-            Estado==4;
-          }else {
-            strcpy(token1.tokens, "Tkn_numero");
-            fwrite (&token1, sizeof(token1), 1, Tokens);
-            Estado==7;
-          }
-          break;
-        case '.':
-            if (Estado==6) {
-              strcpy(token1.tokens, "Punto Decimal");
-              fwrite (&token1, sizeof(token1), 1, Tokens);
-              Estado==7;
-            } else if (Estado==3) {
-              strcpy(token1.tokens, "Punto Decimal");
-              fwrite (&token1, sizeof(token1), 1, Tokens);
-              Estado==4;
-            }
-            break;
-        case (c=='-'||c=='+'||c=='/'||c=='*'):
-              strcpy(token1.tokens, "Tkn_Operador_Aritmetico");
-              fwrite (&token1, sizeof(token1), 1, Tokens);
-            break;
-        case (c=='<'||c=='>'||c=='='||c=='!'):
-              strcpy(token1.tokens, "Tkn_Relacion");
-              fwrite (&token1, sizeof(token1), 1, Tokens);
-            break;
-        case(c=='<'&& c=='-'):
-              strcpy(token1.tokens, "Tkn_Asignacion");
-              fwrite (&token1, sizeof(token1), 1, Tokens);
-            break;
-        case(pal_res.includes(c)):
+    while ((c=fgetc(pf))!=EOF) {
+      for(i=0;i<=16;i++){//Busca en el arreglo pal_res
+        for(j=0;j<=25;j++){
+          if(pal_res[i][j]==c){
             strcpy(token1.tokens, "Tkn_Palabra_Reservada");
             fwrite (&token1, sizeof(token1), 1, Tokens);
-            break;
-        case '\n':
-            if (Estado==0||Estado==1||Estado==2) {
-              strcpy(token1.tokens, "Tkn_cadena");
-              fwrite (&token1, sizeof(token1), 1, Tokens);
-            }else if(Estado==3||Estado==4||Estado==5){
-              strcpy(token1.tokens, "Tkn_Operacion_Aritmetica");
-              fwrite (&token1, sizeof(token1), 1, Tokens);
-          	}else if(Estado==2||Estado==6)){
-              strcpy(token1.tokens, "Tkn_Relacion");
-              fwrite (&token1, sizeof(token1), 1, Tokens);
-          	}else if(Estado==0||Estado==2||Estado==6||Estado==8)){
-              strcpy(token1.tokens, "Tkn_Asignacion");
-              fwrite (&token1, sizeof(token1), 1, Tokens);
-          	}
-            break;
-        default;
-          strcpy(error1.errors, "Tkn_No_Reconocido");
-          fwrite (&error1, sizeof(error1), 1, Errores);
+            Estado=9;
+          }
         }
       }
-      fclose(pf);
-      fclose (Errores);
-      strcpy(token1.tokens, "$");
-      fwrite (&token1, sizeof(token1), 1, Tokens);
-      fclose (Tokens);
-    }
-    return 0;
+      switch (Estado) {
+        case 0:
+          if (c>='A'&&c<='Z'){//Letras Mayusculas
+            strcpy(token1.tokens, "Tkn_LetraMa");
+            fwrite (&token1, sizeof(token1), 1, Tokens);
+              Estado=1;
+            }else if (c=='-'||c=='+'||c=='/'||c=='*'){//Tnk Operadores y Relacion
+                    Estado=3;
+            }else if(c<='0'&&c>='9'){//Numeros
+                Estado=5;
+              }else{
+              strcpy(error1.errors, "Error en el archivo");
+              fwrite (&error1, sizeof(error1), 1, Errores);
+              exit(1);
+            }
+        break;
+        case 1:
+          if ((c>='a'&&c<='z')||(c>='A'&&c<='Z')){//Letras Minusculas
+                Estado==1;
+            }if(c<='0'&&c>='9'){//Numeros
+                Estado=2;
+            }
+        break;
+        case 2:
+          if(c<='0'&&c>='9'){//Numeros
+              Estado=2;
+            }else if ((c=='-'||c=='+'||c=='/'||c=='*')&&(c<='0'&&c>='9')){//Tnk Operadores y Relacion
+                    Estado=6;
+            }else if((c=='<'&&c=='-')&&(c<='0'&&c>='9')){//Tnk de Asignacion
+                  Estado=8;
+            }else if((c=='<'&&c=='-')&&(c<='A'&&c>='Z')){//Tnk de Asignacion
+                  Estado=0;
+            }
+        break;
+        case 3:
+          if(c<='0'&&c>='9'){//Numeros
+            Estado=3;
+          }else if (c=='-'||c=='+'||c=='/'||c=='*'){//Tnk Operadores y Relacion
+                Estado=5;
+          }else if(c=='.'){//Punto Decimal
+                strcpy(token1.tokens, "Punto Decimal");
+                fwrite (&token1, sizeof(token1), 1, Tokens);
+                  Estado=4;
+          }else  if ((c=='-'||c=='+'||c=='/'||c=='*')&&(c<='0'&&c>='9')){//Tnk Operadores y Relacion
+                  Estado=5;
+          }
+        break;
+        case 4:
+          if(c<='0'&&c>='9'){//Numeros
+            Estado=4;
+          }else if ((c=='-'||c=='+'||c=='/'||c=='*')&&(c<='0'&&c>='9')){//Tnk Operadores y Relacion
+                  Estado=5;
+          }else if ((c=='-'||c=='+'||c=='/'||c=='*')&&(c<='A'&&c>='Z')){//Tnk Operadores y Relacion
+                  Estado=0;
+          }
+        break;
+        case 5:
+          if(c<='0'&&c>='9'){//Numeros
+            Estado=3;
+          }else if (c=='-'||c=='+'||c=='/'||c=='*'){//Tnk Operadores y Relacion
+                strcpy(token1.tokens, "Tkn_Operador_Aritmetico");
+                fwrite (&token1, sizeof(token1), 1, Tokens);
+                Estado=0;
+          }
+        break;
+        case 6:
+          strcpy(token1.tokens, "Punto Decimal");
+          fwrite (&token1, sizeof(token1), 1, Tokens);
+          if(c<='0'&&c>='9'){//Numeros
+            Estado=6;
+          }else if(c=='.'){//Punto Decimal
+                Estado=7;
+          }
+        break;
+        case 7:
+          if(c<='0'&&c>='9'){//Numeros
+            Estado=7;
+          }
+        break;
+        case 8:
+        if((c=='<'&&c=='-')&&(c<='0'&&c>='9')){//Tnk de Asignacion
+              Estado=3;
+        }else if((c=='<'&&c=='-')&&(c<='A'&&c>='Z')){//Tnk de Asignacion
+              Estado=0;
+        }
+        break;
+        case 9:
+          if(c<='0'&&c>='9'){//Numeros
+            Estado=8;
+          }else if(c<='A'&&c>='Z'){//Numeros
+            Estado=0;
+          }
+        break;
+      }
+      if( c=='\n'){//Ultimo Tnk Analizado
+          if (Estado==0||Estado==1||Estado==2) {
+            strcpy(token1.tokens, "Tkn_cadena");
+            fwrite (&token1, sizeof(token1), 1, Tokens);
+          }else if(Estado==3||Estado==4||Estado==5){
+            strcpy(token1.tokens, "Tkn_Operacion_Aritmetica");
+            fwrite (&token1, sizeof(token1), 1, Tokens);
+          }else if(Estado==6){
+            strcpy(token1.tokens, "Tkn_Relacion");
+            fwrite (&token1, sizeof(token1), 1, Tokens);
+          }else if(Estado==7){
+            strcpy(token1.tokens, "Tkn_numeroDecimal");
+            fwrite (&token1, sizeof(token1), 1, Tokens);
+
+          }
+        }
+      }
+}
+    fclose(pf);
+    fclose (Errores);
+    strcpy(token1.tokens, "$");
+    fwrite (&token1, sizeof(token1), 1, Tokens);
+    fclose (Tokens);
+  return 0;
   }
